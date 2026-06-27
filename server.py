1757478b86cffb62f801daca54157ca98cc293e8
Nico <n37287935@gmail.com>
2026-06-27 12:21:53 +0200

    Add database connection

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return "Hello World"
+@app.route("/")
+def index ():
+    return ("hello world")
 
-if __name__ == '__main__':
+
+
+if __name__ in "__main__":
     app.run(debug=True)
-
-print("hello world")
